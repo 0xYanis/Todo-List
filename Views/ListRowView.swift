727b081f95063db78e503d92f76a5a11a952da21
Yanis <111576120+0xYanis@users.noreply.github.com>
2022-12-29 15:34:15 +0300

    Add files via upload

---
+++ Views/ListRowView.swift
@@ -0,0 +1,48 @@
+//
+//  ListRowView.swift
+//  TodoList
+//
+//  Created by Yan Rybkin on 19.12.2022.
+//
+
+import SwiftUI
+
+struct ListRowView: View {
+	
+	let item: ItemModel
+	
+	var body: some View {
+		VStack {
+			HStack(spacing: 15) {
+				Circle()
+					.foregroundColor(.white)
+					.frame(width: 30, height: 30)
+					.overlay {
+						Circle().stroke(item.isCompleted ? .green : .red,
+										lineWidth: 7)
+					}
+				
+				Text(item.title)
+				Spacer()
+			}
+			.padding()
+			.background(Color(.white))
+			.cornerRadius(15)
+			.shadow(color: .gray, radius: 10, x:0, y:4)
+		}
+	}
+}
+
+struct ListRowView_Previews: PreviewProvider {
+	
+	static var item1 = ItemModel(title: "First item!", isCompleted: false)
+	static var item2 = ItemModel(title: "Second item!", isCompleted: true)
+	
+    static var previews: some View {
+		Group {
+			ListRowView(item: item1)
+			ListRowView(item: item2)
+		}
+		.previewLayout(.sizeThatFits)
+    }
+}
